5fa87d362e278cfbe1e754693f120129a882b248
Alexander <alexanderslesarevpost@mail.ru>
2024-03-23 16:02:53 +0400

    feat: добавил хук для страницы получения списка дел

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'normalize.css';
 import './styles.css';
-import { List } from '../List/List';
+import { List } from '../../pages/List/List';
 
 export const App: React.FC = () => (
   <div className="App">
